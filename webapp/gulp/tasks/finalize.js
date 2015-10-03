febe1317108f1eb702ba47e5c6a7dff5779a732c
Yassin N. Hassan <hassany@student.ethz.ch>
2015-10-03 18:23:02 +0200

    xx

---
+++ webapp/gulp/tasks/finalize.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var changed    = require('gulp-changed');
+var gulp       = require('gulp');
+var config     = require('../config').finalize;
+var browserSync  = require('browser-sync');
+
+gulp.task('finalize', ['markup', 'images', 'fonts', 'minifyCss', 'uglifyJs'], function() {
+  return gulp.src(config.src)
+    .pipe(changed(config.dest)) // Ignore unchanged files
+    .pipe(gulp.dest(config.dest))
+    .pipe(browserSync.reload({stream:true}));
+});
